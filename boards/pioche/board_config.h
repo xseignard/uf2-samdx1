a21823a5e254752f9927cdd6f9921b28bbd5d556
bigx <bigx@MacBook-Pro-de-bigx-2.local>
2020-11-26 12:10:45 +0100

    wip

---
+++ boards/pioche/board_config.h
@@ -0,0 +1,15 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "Pioche"
+#define PRODUCT_NAME "Corpore Sano"
+#define VOLUME_LABEL "PIOCHE"
+#define INDEX_URL "http://www.pioche.co"
+#define BOARD_ID "CORPORE_SANO"
+
+#define USB_VID 0x1B4F
+#define USB_PID 0x0D22
+
+#define LED_PIN PIN_PA17
+
+#endif
---
+++ boards/pow/board_config.h
@@ -0,0 +1,15 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "POW"
+#define PRODUCT_NAME "POW-SWITCH"
+#define VOLUME_LABEL "POW"
+#define INDEX_URL "https://pow.cool"
+#define BOARD_ID "POW"
+
+#define USB_VID 0x1B4F
+#define USB_PID 0x0D22
+
+#define LED_PIN PIN_PA17
+
+#endif
